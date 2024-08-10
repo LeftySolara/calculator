0b4534bfaa6d4eb6136bcd13fd1e224ef545adce
Julianne Adams <julianne@julianneadams.dev>
2024-08-10 15:51:14 -0400

    Add: stack data structure

---
+++ frontend/src/lib/stack.ts
@@ -0,0 +1,33 @@
+interface IStack<T> {
+  push(item: T): void;
+  pop(): T | undefined;
+  peek(): T | undefined;
+  size(): number;
+}
+
+class Stack<T> implements IStack<T> {
+  private data: T[] = [];
+
+  constructor(private capacity: number = Infinity) {}
+
+  push(item: T): void {
+    if (this.size() === this.capacity) {
+      throw Error("Stack has reached maximum capacity.");
+    }
+    this.data.push(item);
+  }
+
+  pop(): T | undefined {
+    return this.data.pop();
+  }
+
+  peek(): T | undefined {
+    return this.data[this.size() - 1];
+  }
+
+  size(): number {
+    return this.data.length;
+  }
+}
+
+export default Stack;
